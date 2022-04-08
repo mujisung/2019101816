45274d1ab6b882a47d7b13dc4725f166168e3cf7
B <wysgene19@khu.ac.kr>
2022-04-08 10:28:19 +0900

    gugudan.cpp initialy added

---
+++ gugudan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+void gugudan(int n) { // n * n ´Ü
+	for (int i = 1; i < n + 1; i++) {
+		std::cout << "-----------" << i << "´Ü" << "-----------" << '\n';
+		for (int j = 1; j < n + 1; j++) {
+			std::cout << i << " * " << j << " = " << i * j << '\n';
+		}
+		std::cout << '\n';
+	}
+}
+
+int main() {
+	gugudan(8);
+}
